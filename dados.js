adeaaa6c52dc8150adf0a5747f4fef65aa99512e
Arthur Lima Cogo <67808884+ArthurLimaCogo@users.noreply.github.com>
2024-09-09 18:05:22 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "League of Legends",
+      descricao: "League of Legends (LoL) é um jogo multiplayer online battle arena (MOBA) extremamente popular, onde duas equipes de cinco jogadores se enfrentam em mapas temáticos, controlando poderosos campeões com habilidades únicas.",
+      link: "https://www.leagueoflegends.com/pt-br/",
+      tags: "MOBA jogo online multiplayer competitivo e-sports estratégia ação LoL Riot Games champions Summoner's Rift ARAM League Client patch notes meta build counter pick"
+    },
+    {
+      titulo: "Minecraft",
+      descricao: "Minecraft é um jogo de construção de mundo aberto onde os jogadores podem explorar, coletar recursos, construir estruturas e criar ferramentas.",
+      link: "https://www.minecraft.net/",
+      tags: "sandbox mundo aberto construção exploração sobrevivência criativo multiplayer singleplayer indie Mojang Notch blocos crafting redstone mods servers skins realms Ender Dragon"
+    },
+    {
+      titulo: "Mario Kart",
+      descricao: "Mario Kart é uma série de jogos de corrida estrelada pelos personagens da franquia Super Mario, conhecida por seus power-ups e pistas desafiadoras.",
+      link: "https://pt.wikipedia.org/wiki/Mario_Kart",
+      tags: "corrida kart multiplayer party game Nintendo arcade divertido família competitivo Mario Kart Nintendo Switch Wii DS personagens circuitos itens batalha speedrun"
+    }
+  ];
